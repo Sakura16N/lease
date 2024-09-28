3ae305587154977ada9b6e13de8681ac08ea0720
Sakura16N <sakura16nn@163.com>
2024-09-28 19:06:01 +0800

    3nd : 重新配置依赖,测试项目整体架构成功

--- web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
+++ web/web-admin/src/main/java/com/atguigu/lease/AdminWebApplication.java
@@ -12,4 +12,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class, args);
-}
+}}
